df858e0447b641e931aac1402c961353c594e338
NikolayKalyshnuy <nikkalaba245@gmail.com>
2022-12-11 09:52:16 +0200

    asdfg

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 print('hello')
 print('world')
-input("Get number")
 
 with open('outputs/1.txt', 'w'):
     pass
 
 import requests
-req = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=144aad8b326f4a8ede3b1521eeadd6ed"
-requests.get(req)
+
+lat = 44.34
+lon = 10.99
+api_key = ''
+with open('keys.txt', 'r') as file:
+    api_key = file.readline()
+req = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}"
+resp = requests.get(req)
+print(api_key)
+print(resp.text)
