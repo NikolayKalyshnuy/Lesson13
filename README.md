3193d50269f4c6b0289a8a6042e303b92fc11620
NikolayKalyshnuy <nikkalaba245@gmail.com>
2022-12-11 09:37:30 +0200

    qwer

---
+++ README.md
@@ -0,0 +1,5 @@
+# *dvbn*
+
+1. dvbn
+2. fghjkl;
+3. xcvbdry
--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 print('hello')
 print('world')
 input("Get number")
+
+with open('outputs/1.txt', 'w'):
+    pass
+
+import requests
+req = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=144aad8b326f4a8ede3b1521eeadd6ed"
+requests.get(req)
